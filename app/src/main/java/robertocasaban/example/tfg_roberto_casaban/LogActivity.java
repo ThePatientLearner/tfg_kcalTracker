bfe05bf2c0c4cc5838af87f104a330bd2820fe25
Roberto C.C <roberto.casaban@hotmail.com>
2026-03-23 14:09:01 +0100

    xml layouts

---
+++ app/src/main/java/robertocasaban/example/tfg_roberto_casaban/LogActivity.java
@@ -0,0 +1,18 @@
+package robertocasaban.example.tfg_roberto_casaban;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+    }
+}
